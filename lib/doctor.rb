4c19840591e7a18c1fc5d9efd5cd455b009e7048
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-19 14:10:19 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def appointments
+    Appointment.all.select {|apt| apt.doctor == self}
+  end 
+  
+  def patients
+    self.appointments.collect {|apt| apt.patient}
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+end 
